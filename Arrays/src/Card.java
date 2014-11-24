f18ece4901687611ab4748a3e6f910b385db1b5a
darryl <d.amatsetam@gmail.com>
2014-11-24 18:17:21 +0100

    card now passes tests

--- Arrays/src/Card.java
+++ Arrays/src/Card.java
@@ -1,13 +1,16 @@
 public class Card implements Comparable<Card> {
-
+	private final Number num;
+	private final Suit suit;
 
 	/**
 	 * Constructor
-	 * 
-	 * @param number
-	 * @param suit
+	 *
+	 * @param num Waarde van kaart
+	 * @param suit Kleur van kaart
 	 */
 	Card(Number num, Suit suit) {
+		this.num = num;
+		this.suit = suit;
 	}
 
 
@@ -15,14 +18,31 @@ public class Card implements Comparable<Card> {
 	 * Pretty-print deze Card als string
 	 */
 	public String toString() {
-		return "Aas" + " van " + "Ruiten";
+		return num + " van " + suit;
 	}
 
 	/**
 	 * Vergelijk twee kaarten.
 	 */
 	public int compareTo(Card card) {
+		if (getIndex(this.num) > getIndex(card.getNum())) {
+			return 1;
+		} else if (getIndex(this.num) < getIndex(card.getNum())) {
+			return -1;
+		}
 		return 0;
 	}
 
+	private int getIndex(Number num) {
+		Number[] values = Number.values();
+		int index = 0;
+		while (values[index] != num) {
+			index++;
+		}
+		return index;
+	}
+
+	public Number getNum() {
+		return num;
+	}
 }
